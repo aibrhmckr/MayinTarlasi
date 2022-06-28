b2753543b7564c454559a6d9a2199c9828e8d62e
Ali İbrahim ÇAKIR <aibrhmckrofficial@gmail.com>
2022-06-28 14:56:00 +0300

    Oyun büyüklüğü kullanıcıdan alınmaya başlandı

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        MineSweeper mineSweeper = new MineSweeper(4,4);
+        System.out.println("Oyun buyuklugunu giriniz");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Satir buyuklugunu giriniz:");
+        int rowNumber = scanner.nextInt();
+        System.out.print("Sutun buyuklugunu giriniz:");
+        int colNumber = scanner.nextInt();
+        MineSweeper mineSweeper = new MineSweeper(rowNumber,colNumber);
     }
 }
